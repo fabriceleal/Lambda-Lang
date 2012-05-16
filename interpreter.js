6343af4ba58b656a39795aafc4f0e3114a1716a3
Fabrice Leal vmUbuntu <fabrice.leal.ch@gmail.com>
2012-05-16 11:31:01 -0700

    changed the interpreter so it "compiles" functions only when necessary

--- interpreter.js
+++ interpreter.js
@@ -30,9 +30,31 @@ var evalExpr = function(expr, env){
 	if(! expr instanceof Array)
 		throw 'Ups! Expr is not an array ...';
 
+	// Lambda
 	switch(expr[0]){
 		case "#":
-			// TODO Create a nice representation, so you can see it in the console
+			// Return the expr itself. Expr should only be "compiled" into a function in an application
+			return expr;	
+	}
+
+	// Application
+	return (
+			evalExprForce(expr[0], env)
+	)( expr[1] );
+
+	throw 'Ups! Looks like the interpreter is missing something :P ... failed with expr = ' + JSON.stringify(expr);
+};
+
+
+var evalExprForce = function(expr, env){
+	if(env === undefined)
+		env = { bindings:{}, outer:{}};
+
+	if(! expr instanceof Array)
+		return evalExpr(expr, env);
+	
+	switch(expr[0]){
+		case "#":
 			// Return a Javascript function object
 			return function(arg){
 				var new_env = {
@@ -40,16 +62,12 @@ var evalExpr = function(expr, env){
 					outer: env
 				};
 				
-				//expr[1].forEach(function(item, idx){
-				//});
 				addBinding(expr[1], arg, new_env);				
 
 				return evalExpr(expr[2], new_env); 
-			};			
+			};	
 	}
-
-	return (evalExpr(expr[0], env))( expr[1] );
-
-	throw 'Ups! Looks like the interpreter is missing something :P ... failed with expr = ' + JSON.stringify(expr);
+	
+	return evalExpr(expr, env);
 };
 
